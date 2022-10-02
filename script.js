5354e3b1f3b690556e1440acd07d3bda6e02a0ed
Hemant Kumar <63533985+Hemant1101@users.noreply.github.com>
2022-10-03 02:08:54 +0530

    added dark mode functionality

--- script.js
+++ script.js
@@ -13,3 +13,14 @@ setInterval(()=>{
    
    
 },1000);
+const toggle = document.querySelector(".toggle");
+toggle.addEventListener("click", (e) => {
+  const html = document.querySelector("html");
+  if (html.classList.contains("dark")) {
+    html.classList.remove("dark");
+    e.target.innerHTML = "Dark mode";
+  } else {
+    html.classList.add("dark");
+    e.target.innerHTML = "Light mode";
+  }
+});
