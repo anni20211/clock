609fe9eea06a516de87ad14056309438020f7229
Landon Moss <landonmoss0729@gmail.com>
2023-12-20 12:00:48 -0500

    css chnages and javaScript touch up

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 setInterval(()=>{
-   d=new Date();
-   ht=d.getHours();
-   hm=d.getMinutes();
-   hs=d.getSeconds();
-   hrotation= 30*ht+hm/2;
-   mrotation= 6*hm;
-   srotation= 6*hs;
-
-   hour.style.transform=`rotate(${hrotation}deg)`;
-   minute.style.transform=`rotate(${mrotation}deg)`;
-   second.style.transform=`rotate(${srotation}deg)`;
-   
+   //Get current date and time
+    let d=new Date();
+    //Extract minutes,hours, and seconds.
+    let ht=d.getHours();
+    let hm=d.getMinutes();
+    let hs=d.getSeconds();
+    //Calculate rotation angles for the clock hands.
+   let hrotation= 30*ht+hm/2;
+   let mrotation= 6*hm;
+   let srotation= 6*hs;
+   //rotate the hour hand
+  document.getElementById('hour').style.transform = `rotate(${hrotation}deg)`;
+  //rotate the minute hand 
+   minute.style.transform('minute').style.transform = `rotate(${mrotation}deg)`;
+   //rotate the second hand
+   second.style.transform('second').style.transform = `rotate(${srotation}deg)`;
    
 },1000);
